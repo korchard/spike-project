3f6a50082b83e60903642aeffd076381aae19a36
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-01-08 13:41:50 -0600

    got nodemailer to work

--- server/routes/contact.router.js
+++ server/routes/contact.router.js
@@ -1,61 +1,68 @@
 const express = require('express');
-// const pool = require('../modules/pool');
+const pool = require('../modules/pool');
 const router = express.Router();
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // GET ROUTE
 router.get('/', (req, res) => {
     res.sendStatus(200); 
 });
 
-// POST ROUTEE
-// router.post('/', (req, res) => {
-//     console.log('email', req.body);
-    
-//     let sqlText = `INSERT INTO nodemailer (name, email_address, subject, message)
-//                    VALUES ( $1, $2, $3, $4 )`;
-//     pool.query(sqlText, [req.body.name, req.body.email_address, req.body.subject, req.body.message]) 
-//         .then((result) => { 
-//             res.sendStatus(200); 
-//         }).catch(error => {
-//           console.log('Bad news bears, error in POST', error);
-//           res.sendStatus(500)
-//         })
-// });
-
-// NODEMAILER POST
+// NODEMAILER && POST ROUTE
 router.post('/', (req, res) => {
     console.log('email', req.body);
     const data = req.body;
+    const password = process.env.password;
   
     const smtpTransport = nodemailer.createTransport({
-        host: 'gmail',
-        // port: 587,
-        // secure: false,
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
         auth: {
             user: 'kimberly.a.orchard@gmail.com',
-            pass: 'Blake7733'
+            pass: password
+        },
+        tls: {
+            rejectUnauthorized: false 
         }
     });
+
+    smtpTransport.verify(function(error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take our messages!");
+        }
+      });
   
     const mailOptions = {
         from: `${data.email_address}`,
-        to: 'kimberly.a.orchard@gmail.com',
+        to: 'jfredericksen12@gmail.com',
         subject: `${data.subject}`,
         html: `<p>${data.message}</p>
                 <p>Thank you, ${data.name}</p>`
     };
-  
+
     smtpTransport.sendMail(mailOptions,
         (error, response) => {
             if (error) {
-                res.send(error)
+                console.log('error sending', error);
             } else {
-                res.send('Success')
+                console.log('Success!');
             }
             smtpTransport.close();
     });
-  
+
+    let sqlText = `INSERT INTO nodemailer (name, email_address, subject, message)
+                   VALUES ( $1, $2, $3, $4 )`;
+    pool.query(sqlText, [data.name, data.email_address, data.subject, data.message]) 
+        .then((result) => { 
+            res.sendStatus(200); 
+        }).catch(error => {
+          console.log('Bad news bears, error in POST', error);
+          res.sendStatus(500)
+        })
 })
 
 // DELETE ROUTE
