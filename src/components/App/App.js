b4b9b9da40f2f7d3d5c9c2e113b7c59b6fa0b38e
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-01-08 15:08:49 -0600

    removed weebviewer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
+import { Button, ButtonGroup } from '@pdftron/webviewer-react-toolkit';
 import './App.css';
 import Contact from '../Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <main className="app__main">
       <Contact />
+        <div className="app__placeholder">Click Load PDF to begin organizing pages.</div>
+      </main>
+      <footer className="app__footer">
+        <ButtonGroup>
+          <Button buttonStyle="borderless">Load PDF</Button>
+          <Button disabled>Download PDF</Button>
+        </ButtonGroup>
+      </footer>
     </div>
   );
 }
