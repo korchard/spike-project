6d0627c2cfb1c1dca99d442e0ac162efffedbec0
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-01-09 00:03:38 -0600

    removed all the code that was used to setup webviewer with react specifically

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,18 +17,8 @@ function App() {
   return (
     <div className="app">
       <Contact />
-      <div className="webviewer" ref={viewerDiv}>
+      <div className="webviewer" ref={viewerDiv}></div>
 
-      </div>
-      <main className="app__main">
-        <div className="app__placeholder">Click Load PDF to begin organizing pages.</div>
-      </main>
-      <footer className="app__footer">
-        <ButtonGroup>
-          <Button buttonStyle="borderless">Load PDF</Button>
-          <Button disabled>Download PDF</Button>
-        </ButtonGroup>
-      </footer>
     </div>
   );
 }
