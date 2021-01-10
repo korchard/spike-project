d772d5268596a4457e32ba917eea829f78788841
Kimberly Orchard <kimberly.a.orchard@gmail.com>
2021-01-10 13:00:06 -0600

    created a get route for the shipping cost

--- src/components/USPS/USPS.js
+++ src/components/USPS/USPS.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
-// const XMLParser = require('react-xml-parser');
-// const xml = new XMLParser().parseFromString();    // Assume xmlText contains the example XML
-// console.log(xml);
-// console.log(xml.getElementsByTagName('Name'));
+const XMLParser = require('react-xml-parser');
+const xml = new XMLParser().parseFromString();    // Assume xmlText contains the example XML
 
 class USPS extends Component {
 
@@ -17,7 +15,21 @@ class USPS extends Component {
   calculateShipping = () => {
     console.log('GETTING SHIPPING!');
 
-    axios.get('https://secure.shippingapis.com/ShippingAPI.dllAPI=RateV4&amp;XML=')
+    axios.get(`https://secure.shippingapis.com/ShippingAPI.dllAPI=RateV4&amp;XML=
+            <RateV4Request USERID="853TOWAN2209">
+            <Revision>2</Revision>
+            <Package ID="1ST">
+                <Service>PRIORITY</Service>
+                <ZipOrigination>55127</ZipOrigination>
+                <ZipDestination>55407</ZipDestination>
+                <Pounds>3</Pounds>
+                <Ounces>2</Ounces>
+                <Container>VARIABLE</Container>
+                <Width>5</Width>
+                <Length>7</Length>
+                <Height>4</Height>
+                </Package>
+            </RateV4Request>`)
     .then((response) => {
       console.log('Got the things...', response.data);
       this.setState({
